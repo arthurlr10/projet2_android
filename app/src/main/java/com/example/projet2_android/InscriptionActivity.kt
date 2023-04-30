6a14ef5a3c782944f005e41777f117d5e585ccf2
LORY Arthur <arthur.lory@epsi.fr>
2023-04-30 11:51:18 +0200

    menu avec deux boutons pour inscription et formulaire d'inscription dans sharedPref

---
+++ app/src/main/java/com/example/projet2_android/InscriptionActivity.kt
@@ -0,0 +1,71 @@
+package com.example.projet2_android
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class InscriptionActivity : AppCompatActivity() {
+
+    private lateinit var sharedPref: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inscription)
+
+        sharedPref = getSharedPreferences("inscription", Context.MODE_PRIVATE)
+
+        val nomEditText = findViewById<EditText>(R.id.nom_edittext)
+        val prenomEditText = findViewById<EditText>(R.id.prenom_edittext)
+        val adresseEditText = findViewById<EditText>(R.id.adresse_edittext)
+        val codePostalEditText = findViewById<EditText>(R.id.code_postal_edittext)
+        val villeEditText = findViewById<EditText>(R.id.ville_edittext)
+        val carteFideliteEditText = findViewById<EditText>(R.id.carte_fidelite_edittext)
+
+        val inscriptionButton = findViewById<Button>(R.id.inscription_button)
+        inscriptionButton.setOnClickListener {
+            val nom = nomEditText.text.toString()
+            val prenom = prenomEditText.text.toString()
+            val adresse = adresseEditText.text.toString()
+            val codePostal = codePostalEditText.text.toString()
+            val ville = villeEditText.text.toString()
+            val carteFidelite = carteFideliteEditText.text.toString()
+
+            with (sharedPref.edit()) {
+                putString("nom", nom)
+                putString("prenom", prenom)
+                putString("adresse", adresse)
+                putString("code_postal", codePostal)
+                putString("ville", ville)
+                putString("carte_fidelite", carteFidelite)
+                apply()
+            }
+
+            Toast.makeText(this, "Inscription réussie!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent) // Démarrer l'activité HomeActivity
+            finish() // Terminer l'activité InscriptionActivity
+        }
+
+        // Récupérer les données d'inscription si elles existent
+        val nom = sharedPref.getString("nom", "")
+        val prenom = sharedPref.getString("prenom", "")
+        val adresse = sharedPref.getString("adresse", "")
+        val codePostal = sharedPref.getString("code_postal", "")
+        val ville = sharedPref.getString("ville", "")
+        val carteFidelite = sharedPref.getString("carte_fidelite", "")
+
+        nomEditText.setText(nom)
+        prenomEditText.setText(prenom)
+        adresseEditText.setText(adresse)
+        codePostalEditText.setText(codePostal)
+        villeEditText.setText(ville)
+        carteFideliteEditText.setText(carteFidelite)
+    }
+}
+
+
--- app/src/main/java/com/example/projet2_android/MainActivity.kt
+++ app/src/main/java/com/example/projet2_android/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.projet2_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scanButton = findViewById<Button>(R.id.scan_button)
+        scanButton.setOnClickListener {
+        }
+
+        val registerButton = findViewById<Button>(R.id.register_button)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, InscriptionActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
